817994b33030075fa3ee86616e506a8860cdf937
srikanthreddy9 <84518754+srikanthreddy9@users.noreply.github.com>
2022-05-15 18:29:55 +0530

    n,m=map(int,input().split()) l=list(map(int,input().split())) s=list(map(int,input().split())) e=[] f=[] c=0 for i in range(n):     if l[i] not in e:         e.append(l[i]) for j in range(m):     if s[j] not in f:         f.append(s[j]) for k in range(len(e)):     if e[k] not in f:         c+=1 for g in range(len(f)):     if f[g] not in e:         c+=1 print(c)

---
+++ Count_elements_II.py
@@ -0,0 +1,20 @@
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+s=list(map(int,input().split()))
+e=[]
+f=[]
+c=0
+for i in range(n):
+    if l[i] not in e:
+        e.append(l[i])
+for j in range(m):
+    if s[j] not in f:
+        f.append(s[j])
+for k in range(len(e)):
+    if e[k] not in f:
+        c+=1
+for g in range(len(f)):
+    if f[g] not in e:
+        c+=1
+print(c)
+        
